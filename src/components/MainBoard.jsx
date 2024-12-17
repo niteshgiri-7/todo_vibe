b00843cae0e9378cc16a69693f4315e85d2596a5
niteshgiri-7 <niteshgiri1717@gmail.com>
2024-12-17 13:06:52 +0545

    logs removed

--- src/components/MainBoard.jsx
+++ src/components/MainBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import NavBar from "./NavBar";
 import Cards from "./Cards";
 import Shimmer from "./Shimmer";
